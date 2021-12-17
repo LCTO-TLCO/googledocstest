d548f8a60b29f5789a08c4cbfc3b8a173102e876
LCTO-TLCO <25495545+LCTO-TLCO@users.noreply.github.com>
2021-12-17 15:26:32 +0900

    modify file

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -19,6 +19,6 @@ res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com
 rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
 # upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
-  --header 'Content-Type: application/pdf'\
-  --upload-file main.pdf
+#curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
+#  --header 'Content-Type: application/pdf'\
+#  --upload-file main.pdf
